e291b3e13a2bfcaa7f93c42019bcebf8bff6688d
ArturoDent <31813701+ArturoDent@users.noreply.github.com>
2019-09-24 20:27:52 -0500

    2019-09-21 Venezuela update -mediaQuery for iPad landscape - added fuzzy searching

--- temp/js/numList.js
+++ temp/js/numList.js
@@ -3,7 +3,7 @@
 
 var numFamiliesList = {
 
-  "Argentina": 87, "Aruba": 52, "Bolivia": 78, "Brazil": 93, "Chile": 68,
+  "Argentina": 87, "Aruba": 52, "Bolivia": 78, "Brazil": 94, "Chile": 68,
   "Colombia": 93, "Curacao": 51, "Ecuador": 93, "French Guiana": 83,
   "Guyana": 80, "Paraguay": 72, "Peru": 89, "Suriname": 81, "Trinidad": 71,
   "Uruguay": 73, "Venezuela": 90, "Bonaire": 48, "Falklands": 49, "South America": 105
@@ -11,10 +11,10 @@ var numFamiliesList = {
 
 var numSpeciesList = {
 
-  "Argentina": 1006, "Aruba": 219, "Bolivia": 1384, "Brazil": 1806, "Chile": 498,
-  "Colombia": 1851, "Curacao": 217, "Ecuador": 1635, "French Guiana": 699,
-  "Guyana": 785, "Paraguay": 694, "Peru": 1802, "Suriname": 732, "Trinidad": 470,
-  "Uruguay": 448, "Venezuela": 1386, "Bonaire": 208, "Falklands": 227, "South America": 3413
+  "Argentina": 1006, "Aruba": 219, "Bolivia": 1384, "Brazil": 1818, "Chile": 505,
+  "Colombia": 1851, "Curacao": 217, "Ecuador": 1635, "French Guiana": 698,
+  "Guyana": 784, "Paraguay": 694, "Peru": 1802, "Suriname": 731, "Trinidad": 470,
+  "Uruguay": 448, "Venezuela": 1391, "Bonaire": 208, "Falklands": 227, "South America": 3413
 };
 
 // numSpecies does not include hypotheticals, from http://www.museum.lsu.edu/~Remsen/SACCCountryLists.htm
