d0ad396e88ff0348ee2e613e4758d8d8a7544da7
ArturoDent <31813701+ArturoDent@users.noreply.github.com>
2024-02-24 20:12:05 -0700

    - 2023-01-24 taxonomy update

--- temp/js/numList.js
+++ temp/js/numList.js
@@ -4,7 +4,7 @@
 var numFamiliesList = {
 
 		"Argentina": 89, "Aruba": 54, "Bolivia": 80, "Brazil": 96, "Chile": 70,
-  "Colombia": 94, "Curaçao": 51, "Ecuador": 94, "French Guiana": 85,
+  "Colombia": 95, "Curaçao": 51, "Ecuador": 94, "French Guiana": 85,
   "Guyana": 81, "Paraguay": 72, "Peru": 91, "Suriname": 83, "Trinidad": 73,
   "Uruguay": 75, "Venezuela": 91, "Bonaire": 48, "Falklands": 51, "South America": 107
 };
@@ -12,7 +12,7 @@ var numFamiliesList = {
 var numSpeciesList = {
 
 		"Argentina": 1043, "Aruba": 245, "Bolivia": 1407, "Brazil": 1859, "Chile": 525,
-  "Colombia": 1869, "Curaçao": 217, "Ecuador": 1659, "French Guiana": 699,
+  "Colombia": 1902, "Curaçao": 217, "Ecuador": 1662, "French Guiana": 699,
   "Guyana": 786, "Paraguay": 694, "Peru": 1865, "Suriname": 744, "Trinidad": 490,
   "Uruguay": 501, "Venezuela": 1402, "Bonaire": 208, "Falklands": 218, "South America": 3476
 };
